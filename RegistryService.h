0fffaf6f74e6b909269d9c67791c38fac72f13a0
Enobong Adahada <adahadapato@gmail.com>
2024-06-16 18:55:48 +0100

    Added_registry_service_class

--- RegistryService.h
+++ RegistryService.h
@@ -1,14 +1,4 @@
 #pragma once
-
-#include "RegistryHLP.h"
-#include <string>
-
-
-#define REGISTRY_NECOSCAN_SUBKEY_EXAMYEAR TEXT("ExamYear")
-#define REGISTRY_NECOSCAN_SUBKEY_EXAMTYPE TEXT("ExamType")
-#define REGISTRY_NECOSCAN_SUBKEY_LOGOUT TEXT("Logout")
-
-
 class RegistryService
 {
 public:
